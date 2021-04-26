0d0ad9672f4b4161f9babf562a272bf4d3fd34cc
LeThiLy <lethily.cntt13e@gmail.com>
2021-04-26 20:17:29 +0700

    Add lần đầu

---
+++ Tuan2/app/src/main/java/com/example/tuan2_a/Tuan2_b.java
@@ -0,0 +1,14 @@
+package com.example.tuan2_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tuan2_b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan2_b);
+    }
+}
---
+++ Tuan2/app/src/main/java/com/example/tuan2_a/Tuan2_c.java
@@ -0,0 +1,14 @@
+package com.example.tuan2_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tuan2_c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan2_c);
+    }
+}
